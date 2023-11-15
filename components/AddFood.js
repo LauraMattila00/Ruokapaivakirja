f9801db49d56eb86a1d4112adb6c0d9e2b9597be
Saija <saija.ristolainen@gmail.com>
2023-11-15 16:11:09 +0200

    Test

--- components/AddFood.js
+++ components/AddFood.js
@@ -4,7 +4,7 @@ import { Text, View } from "react-native"
 export default AddFood = () => {
     return (
         <View>
-            <Text>MOI</Text>
+            <Text>MOI!</Text>
         </View>
     )
 }
