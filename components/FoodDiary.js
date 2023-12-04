1a111f1423ca4fb5f938d09ee2d0048b2d924aa6
saijaris <saija.ristolainen@gmail.com>
2023-12-04 11:35:39 +0200

    Fix calendar

--- components/FoodDiary.js
+++ components/FoodDiary.js
@@ -27,7 +27,7 @@ export default FoodDiary = () => {
     const nextDay = () => {
         const oneDayInMilliseconds = 1000 * 60 * 60 * 24
         const newDate = new Date(selectedDate.getTime() + oneDayInMilliseconds)
-        setSelectedDate(newDate.toLocaleDateString())
+        setSelectedDate(newDate)
     }
 
     const previousDay = () => {
